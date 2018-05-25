559c9afc8e2fd3a7f6d2f0d93fb1e14f93c35d32
Bri <bherreras23@gmail.com>
2018-05-25 01:31:50 -0700

    Bug Fixes

--- index.js
+++ index.js
@@ -1,38 +1,7 @@
-<<<<<<< HEAD
-var Word = require('./word.js');
-var inquirer = require('inquirer');
+var Game = require("./library/Game");
 
-var playWords = [];
+// Initialize a new Game object
+var game = new Game();
 
-var guessCount = 10;
-var gameWord = new Word(playWords[Math.floor(Math.random() * playWords.length)]);
-
-function roundNumber(guessCount, gameWord) {
-	inquirer.prompt([
-		{
-			type: "input",
-			name: "guess",
-			message: "Guess a letter!"
-		}
-
-//]).then(function (results) 
-=======
-var Word = require('./word.js');
-var inquirer = require('inquirer');
-
-var playWords = [];
-
-var guessCount = 10;
-var gameWord = new Word(playWords[Math.floor(Math.random() * playWords.length)]);
-
-function roundNumber(guessCount, gameWord) {
-	inquirer.prompt([
-		{
-			type: "input",
-			name: "guess",
-			message: "Guess a letter!"
-		}
-
-//]).then(function (results) 
->>>>>>> ba5a83b7a192159f057949704c4c3764f0a4d79a
-// Not really sure where to go from here...... definately need some more time!
+// Start playing
+game.play();
